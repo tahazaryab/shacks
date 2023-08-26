26866fdc0c436dda93634cfe0be333205ea258f0
Taha Zaryab <tahazaryab10@gmail.com>
2023-08-25 22:56:22 -0400

    Create layout for nav

--- src/pages/past-reports/index.tsx
+++ src/pages/past-reports/index.tsx
@@ -1,12 +1,13 @@
+import Layout from '../../components/layout/Layout';
 import Table from '../../components/table/Table';
 
 const PastReports = (): JSX.Element => {
   return (
-    <>
+    <Layout>
       <div className="p-20">
         <Table />
       </div>
-    </>
+    </Layout>
   );
 };
 
