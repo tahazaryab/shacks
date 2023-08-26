6c9213b50dc753e82148e7995b02a206df088dae
Taha Zaryab <tahazaryab10@gmail.com>
2023-08-25 20:22:58 -0400

    Install prettier for code formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		colors: {
-			black: "#1D1D1D",
-			white: "#FFFFFF",
-			red: {
-				100: "#ed0722",
-			},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      black: "#1D1D1D",
+      white: "#FFFFFF",
+      red: {
+        100: "#ed0722",
+      },
       blue: {
-				100: "#004591",
-			},
-		},
-		extend: {
-			fontFamily: {
-				poppins: ["Poppins", "sans-serif"],
-			},
-		},
-	},
-	plugins: [],
+        100: "#004591",
+      },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
 };
