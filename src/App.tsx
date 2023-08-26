0592868c4204740fe31e0800dfc2c275294e1cfa
Aayaan <66046533+AayaanN@users.noreply.github.com>
2023-08-26 01:12:55 -0400

    added route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../src/pages/main';
+import SubmitReport from './pages/submit-report/report';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/submit-report" element={<SubmitReport />} />
       </Routes>
     </BrowserRouter>
   );
