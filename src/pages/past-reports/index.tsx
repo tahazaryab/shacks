abba67e482011d010873249fb7b85caf82764efa
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-08-25 22:58:06 -0400

    implemented footer

--- src/pages/past-reports/index.tsx
+++ src/pages/past-reports/index.tsx
@@ -1,19 +1,18 @@
 import ProgressBar from "../../components/progress-bar";
 import { NavbarDefault } from "../../components/navbar";
+import { FooterWithLogo } from "../../components/footer";
 const PastReports = (): JSX.Element => {
     const checkpoints = [
         'Ticket Created', 'Investigated', 'Esclalation', 'Resolution', 'Complete'
       ];
-    
-
-
-
     return <div>
         <NavbarDefault></NavbarDefault>
         <h1>
             hello
         </h1>
         <ProgressBar checkpoints={checkpoints} progress={2}/>
+
+        <FooterWithLogo></FooterWithLogo>
     </div>;
   };
   
