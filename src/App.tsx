499dc1b30b92e61568a0dc9b420e78eeae713339
Yaser <yayonoss@gmail.com>
2023-08-26 10:50:03 -0400

    Create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../src/pages/main';
+import Login from '../src/pages/login';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      black: '#1D1D1D',
-      white: '#FFFFFF',
-      red: {
-        100: '#ed0722',
-      },
-      blue: {
-        100: '#004591',
-      },
-    },
     extend: {
+      colors: {
+        black: '#1D1D1D',
+        white: '#FFFFFF',
+        red: {
+          100: '#ed0722',
+        },
+        blue: {
+          100: '#004591',
+        },
+        // Add all the default Tailwind CSS colors here
+        // You can find the full list in the official Tailwind documentation
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
