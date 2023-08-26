bbc678e51a65652781cc46c1ca5292ae9f0665e4
Kevin Le <74465053+kevinle623@users.noreply.github.com>
2023-08-25 22:06:28 -0400

    progress bar component

--- src/pages/past-reports/index.tsx
+++ src/pages/past-reports/index.tsx
@@ -1,1 +1,19 @@
-export default {};
+import ProgressBar from "../../components/progress-bar";
+const PastReports = (): JSX.Element => {
+    const checkpoints = [
+        'Ticket Created', 'Investigated', 'Esclalation', 'Resolution', 'Complete'
+      ];
+    
+
+
+
+    return <div>
+        <h1>
+            hello
+        </h1>
+        <ProgressBar checkpoints={checkpoints} progress={2}/>
+    </div>;
+  };
+  
+  export default PastReports;
+  
